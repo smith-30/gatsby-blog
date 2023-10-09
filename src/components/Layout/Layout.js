aad3c2e7b4225397e04971a9ef46dee67b9111aa
smith30 <parkour12019@gmail.com>
2023-10-09 17:02:40 +0900

    mod: Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,14 @@
 // @flow
-import React from 'react';
-import Helmet from 'react-helmet';
-import type { Node as ReactNode } from 'react';
-import styles from './Layout.module.scss';
-import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+import React from "react";
+import Helmet from "react-helmet";
+import type { Node as ReactNode } from "react";
+import styles from "./Layout.module.scss";
+import "prismjs/plugins/line-numbers/prism-line-numbers.css";
 
 type Props = {
   children: ReactNode,
   title: string,
-  description?: string
+  description?: string,
 };
 
 const Layout = ({ children, title, description }: Props) => (
@@ -20,6 +20,11 @@ const Layout = ({ children, title, description }: Props) => (
       <meta property="og:site_name" content={title} />
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:title" content={title} />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2255927023702252"
+        crossorigin="anonymous"
+      ></script>
     </Helmet>
     {children}
   </div>
